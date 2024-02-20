2e1beeacef922bf8ed40ac897f5bdcc85b027aa6
GKN-83 <71436569+GKN-83@users.noreply.github.com>
2024-02-20 20:49:18 +0100

    3.2 outlier

--- #4-5 Assignment/test.py
+++ #4-5 Assignment/test.py
@@ -2,34 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
-from prince import FAMD
+import prince
 # Load the dataset
 df = pd.read_csv('police_shooting_anonymized.csv')
 
 
 # Initialize FAMD object: specifying the number of components (n_components)
-famd = FAMD(n_components=2, n_iter=3, random_state=42)
-
+famd = prince.FAMD(
+    n_components=2,
+    n_iter=3,
+    copy=True,
+    check_input=True,
+    random_state=42,
+    engine="sklearn",
+    handle_unknown="error"  # same parameter as sklearn.preprocessing.OneHotEncoder
+)
 # Fit FAMD on the dataset
 famd = famd.fit(df)
+print(famd.eigenvalues_summary)
+print(famd.row_coordinates(df).head())
+print(famd.column_coordinates_)
 
-# Transform the dataset
-df_transformed = famd.transform(df)
-
-
-# Assuming 'df_transformed' is the DataFrame obtained after applying FAMD
-z_scores = np.abs(stats.zscore(df_transformed))
-outliers = np.where(z_scores > 3)
-
-# 'outliers' now contains the indices of the outliers in the transformed dataset
-# Assuming outliers are in rows
-outlier_rows = df.iloc[outliers[0]]
-outlier_rows.to_csv('outliers.csv', index=False)
-
-# Plotting the transformed dataset
-plt.figure(figsize=(10, 7))
-plt.scatter(df_transformed[0], df_transformed[1], alpha=0.5)
-plt.xlabel('First Principal Component')
-plt.ylabel('Second Principal Component')
-plt.title('FAMD - Reduced Dimensionality Visualization')
-plt.show()
+famd.plot(
+    df,
+    x_component=0,
+    y_component=1
+)
